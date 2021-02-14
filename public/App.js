c68df22eb17493685170469a1dd2fe30bb0d83b1
Jamin Ewell <jamin.ewell@yahoo.com>
2021-02-13 23:57:19 -0800

    changes and untrack node

---
+++ public/App.js
@@ -0,0 +1,8 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Jamin Ewell"), /*#__PURE__*/React.createElement("img", {
+  src: "imgs/profile.jpeg"
+}), /*#__PURE__*/React.createElement("p", null, "Hi, my name is Jamin.  I am a Software Developer currently residing in the San Diego area. I enjoy going to the beach and eating seafood in my spare time. I look forward to learning awesome things in this course!"), /*#__PURE__*/React.createElement("a", {
+  href: "https://github.com/JaminEwell"
+}, "VIEW MY GITHUB REPO"));
+ReactDOM.render(element, document.getElementById('contents'));
